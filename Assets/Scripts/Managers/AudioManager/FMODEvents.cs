747a5ba367561c52e785efaab251d573d32052e3
Tuan Le <tuanle9333@gmail.com>
2026-07-23 20:08:43 -0700

    Added Sfx

--- Assets/Scripts/Managers/AudioManager/FMODEvents.cs
+++ Assets/Scripts/Managers/AudioManager/FMODEvents.cs
@@ -7,51 +7,43 @@ using UnityEngine;
 [SuppressMessage("ReSharper", "Odin.OdinUnknownGroupingPath")]
 public class FMODEvents : MonoSingleton<FMODEvents>
 {
+	#region Ambience
+
+	[field: SerializeField]
+	[field: FoldoutGroup("Ambience", expanded: true)]
+	public EventReference Ambience_Amb { get; private set; }
+
+	#endregion
+
 	#region Music
 
 	// [field: SerializeField]
-	// [field: FoldoutGroup("Music", true)]
-	// public EventReference WindowsXP_Bgm { get; private set; }
+	// [field: FoldoutGroup("Music", expanded: true)]
+	// public EventReference Bgm { get; private set; }
 
 	#endregion
 
-	#region Ambience
-
-	// [field: SerializeField]
-	// [field: FoldoutGroup("Ambience", expanded: true)]
-	// public EventReference Ambience_Amb { get; private set; }
-
-	#endregion
-
-	#region SFX
+	#region Elevator Noises
 
 	[field: SerializeField]
-	[field: FoldoutGroup("SFX")]
-	public EventReference Footsteps_Sfx { get; private set; }
+	[field: FoldoutGroup("Elevator Noises", expanded: true)]
+	public EventReference ElevatorOpen_Sfx { get; private set; }
 
 	[field: SerializeField]
-	[field: FoldoutGroup("SFX")]
-	public EventReference Sandsteps_Sfx { get; private set; }
+	[field: FoldoutGroup("Elevator Noises", expanded: true)]
+	public EventReference ElevatorClose_Sfx { get; private set; }
 
 	[field: SerializeField]
-	[field: FoldoutGroup("SFX")]
-	public EventReference Jump_Sfx { get; private set; }
-
-	[field: SerializeField]
-	[field: FoldoutGroup("SFX")]
-	public EventReference ButtonClick_Sfx { get; private set; }
-
-	[field: SerializeField]
-	[field: FoldoutGroup("SFX")]
-	public EventReference Dialogue_Sfx { get; private set; }
+	[field: FoldoutGroup("Elevator Noises", expanded: true)]
+	public EventReference ElevatorDescend_Sfx { get; private set; }
 
 	#endregion
 
 	#region Looping SFX
 
-	[field: SerializeField]
-	[field: FoldoutGroup("Loop SFX", true)]
-	public EventReference Falling_LoopSfx { get; private set; }
+	// [field: SerializeField]
+	// [field: FoldoutGroup("Loop SFX", true)]
+	// public EventReference Falling_LoopSfx { get; private set; }
 
 	#endregion
 }
